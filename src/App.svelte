d5bc12986ee529fd469b6e20b84642090e774a5c
Ray Meredith <ray.meredith@gmail.com>
2019-11-07 16:46:56 -0600

    early attempt at drag-and-drop/svelte combo

---
+++ src/App.svelte
@@ -0,0 +1,60 @@
+<script>
+	export let name;
+	// @ts-ignore
+	import Draggable from 'draggable';
+
+let dropzones = document.querySelectorAll('.dropzone');
+ 
+let droppable = new Draggable.Droppable(
+  dropzones,
+  {
+    draggable: '.draggable',
+    dropzone:  '.dropzone',
+    mirror:    { constrainDimensions: true }
+  }
+);
+</script>
+
+<style>
+  body
+  {
+    display: flex;
+  }
+
+  .dropzone
+  {
+    background: #EEE;
+    border:     8px solid black;
+    display:    block;
+    margin:     16px;
+    min-width:  250px;
+    padding:    16px;
+  }
+
+  .dropzone#alpha { border-color: orangered; }
+  .dropzone#beta  { border-color: green;     }
+  .dropzone#delta { border-color: skyblue;   }
+
+  .draggable
+  {
+    background:  #AAA;
+    cursor:      move;
+    font-weight: bold;
+    list-style:  none;
+    margin:      4px;
+    padding:     8px;
+  }
+</style>
+
+
+<div class="dropzone" id="alpha">
+  <div class='draggable'>Draggable</div>
+</div>
+
+<div class="dropzone" id="beta">
+
+</div>
+
+<div class="dropzone" id="delta">
+
+</div>
